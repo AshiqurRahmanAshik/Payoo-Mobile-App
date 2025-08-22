30770c1bc3529bf02385e39b653c5d182b800d6e
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-08-22 23:34:00 +0600

    Functional Amount and Pin Number Validation

--- home.js
+++ home.js
@@ -1,15 +1,23 @@
 const addMoneyBtn = document.getElementById("add-money-btn");
 addMoneyBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  const amountToAdd = document.getElementById("amount-to-add");
-  const amountToAddValue = amountToAdd.value;
-  const amountToAddConverted = parseInt(amountToAddValue);
-  console.log(amountToAddConverted);
-  let totalAmountElement = document.getElementById("total-amount");
-  let totalAmount = totalAmountElement.innerText;
-  let totalAmountConverted = parseInt(totalAmount);
-  console.log(totalAmountConverted);
-  let remainAmount = totalAmountConverted - amountToAddConverted;
-  totalAmountElement.innerText = remainAmount;
-  
+  const mobileNumber = document.getElementById("mobile-number");
+  const valueOfMobileNumber = mobileNumber.value;
+  const lengthOfMobileNumber = valueOfMobileNumber.length;
+  const pinNumber = document.getElementById("pin-number");
+  const valueOfPin = pinNumber.value;
+  if (lengthOfMobileNumber !== 11 || valueOfPin != 1234) {
+    alert("Invalid Credentials");
+  } else {
+    const amountToAdd = document.getElementById("amount-to-add");
+    const amountToAddValue = amountToAdd.value;
+    const amountToAddConverted = parseInt(amountToAddValue);
+    console.log(amountToAddConverted);
+    let totalAmountElement = document.getElementById("total-amount");
+    let totalAmount = totalAmountElement.innerText;
+    let totalAmountConverted = parseInt(totalAmount);
+    console.log(totalAmountConverted);
+    let remainAmount = totalAmountConverted + amountToAddConverted;
+    totalAmountElement.innerText = remainAmount;
+  }
 });

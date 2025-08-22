15422916d874ce19986a1c79fb84b1f9543d8e5b
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-08-22 19:26:08 +0600

    Payoon Login Page Setup

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const loginBtn = document.getElementById("login-btn");
-loginBtn.addEventListener("click", function (event) {
+const loginButton = document.getElementById("login-btn");
+loginButton.addEventListener("click", function (e) {
   event.preventDefault();
-  const accountNumber = document.getElementById("mobile-number").value;
-  const pin = document.getElementById("pin").value;
-  const convertedPin = parseInt(pin);
-  if (accountNumber.length === 11) {
-    if (convertedPin == 1234) {
-      window.location.href = "./main.html";
-    } else {
-      alert("Invalid Pin Number");
-    }
+  const mobileNumber = document.getElementById("mobile-number");
+  const valueOfMobileNumber = mobileNumber.value;
+  const lengthOfMobileNumber = valueOfMobileNumber.length;
+  const pinNumber = document.getElementById("pin-number");
+  const valueOfPin = pinNumber.value;
+  if (lengthOfMobileNumber == 11 && valueOfPin == 1234) {
+    console.log("Correct");
   } else {
-    alert("Invalid Account Number");
+    console.log("Wrong");
   }
 });

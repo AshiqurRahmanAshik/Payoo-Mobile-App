595d61924a42f62dc90829e60ea16be26a995b0a
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-08-24 13:09:10 +0600

    Add Money and Cash Out Section

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+// Function to toggle feature
 function dashboard(id1, id2) {
   const addMoneyDashboard = document.getElementById(id1);
   addMoneyDashboard.addEventListener("click", function () {
@@ -18,7 +19,66 @@ function dashboard(id1, id2) {
 }
 dashboard("dashboard-add-money", "add-money-form");
 dashboard("dashboard-cash-out", "cash-out-form");
+
+// Function to get innerText in number
+function getInnerTextNumber(id) {
+  const getInnerTextNumber = parseInt(document.getElementById(id).innerText);
+  return getInnerTextNumber;
+}
+
+// Function to get value in number
+function getValueNumber(id) {
+  const getValueNumber = parseInt(document.getElementById(id).value);
+  return getValueNumber;
+}
+
+// Add Money
+function addMoneyButton(id) {
+  const button = document.getElementById(id);
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
+    const addMoneyAccNumber = getValueNumber("add-money-account");
+    const addMoneyPin = getValueNumber("add-money-pin");
+    if (addMoneyAccNumber != 123456789 || addMoneyPin != 1234) {
+      alert("Invalid Credentials");
+      return;
+    }
+    let availableBalance = getInnerTextNumber("total-amount");
+    const addMoney = getValueNumber("add-money-amount");
+    const netBalance = availableBalance + addMoney;
+    document.getElementById("total-amount").innerText = netBalance;
+  });
+}
+addMoneyButton("add-money-submit");
+
+// Cash Out
+function cashOutButton(id) {
+  const button = document.getElementById(id);
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
+    const cashOutAccNumber = getValueNumber("cash-out-agent");
+    const cashOutPin = getValueNumber("cash-out-pin");
+    if (cashOutAccNumber != 123456789 || cashOutPin != 1234) {
+      alert("Invalid Credentials");
+      return;
+    }
+    let availableBalance = getInnerTextNumber("total-amount");
+    const cashOutMoney = getValueNumber("cash-out-amount");
+    const netBalance = availableBalance - cashOutMoney;
+    if (netBalance < 0) {
+      alert("Invalid Credentials");
+      return;
+    }
+    document.getElementById("total-amount").innerText = netBalance;
+  });
+}
+cashOutButton("cash-out-submit");
+
+// Transfer Money
 dashboard("dashboard-transfer", "transfer-money-form");
+// Bonus Money
 dashboard("dashboard-bonus", "bonus-money");
+// Pay Bill
 dashboard("dashboard-pay-bill", "pay-bill-form");
+//  Transactions
 dashboard("dashboard-transactions", "transactions-form");
